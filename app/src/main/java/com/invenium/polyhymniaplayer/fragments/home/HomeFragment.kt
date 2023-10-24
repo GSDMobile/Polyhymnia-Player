71281825f0c570bdf45bcc349bb9a04ef69a1795
Ugur Denli <ugur6@hotmail.co.uk>
2023-10-24 10:01:28 +0100

    Setup for XML
    Setup for XML

--- app/src/main/java/com/invenium/polyhymniaplayer/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/invenium/polyhymniaplayer/fragments/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.invenium.polyhymniaplayer.fragments.home
 
 import androidx.fragment.app.Fragment
+import com.invenium.polyhymniaplayer.R
 
-class HomeFragment : Fragment()
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
