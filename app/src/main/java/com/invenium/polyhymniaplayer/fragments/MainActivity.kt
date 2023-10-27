9c1b66c6cfc48214724fa784d97f7338c4947c27
Ugur Denli <ugur6@hotmail.co.uk>
2023-10-27 11:14:08 +0100

    Fixed navigation
    Fixed navigation

--- app/src/main/java/com/invenium/polyhymniaplayer/fragments/MainActivity.kt
+++ app/src/main/java/com/invenium/polyhymniaplayer/fragments/MainActivity.kt
@@ -3,18 +3,11 @@ package com.invenium.polyhymniaplayer.fragments
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.invenium.polyhymniaplayer.R
-import com.invenium.polyhymniaplayer.fragments.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, HomeFragment())
-                .commit()
-        }
     }
 }
